a27ad6e123f91e6960c396e71b3164eb58eac6f3
shuangxijiang <shuangxijiang@kingnet.com>
2020-09-01 11:44:03 +0800

    feat(排序): 增加冒泡，插入，选择等排序算法

---
+++ php/algorithmic/Sorts/Sort.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiangsx
+ * Date: 2020/8/26 0026
+ * Time: 下午 3:33
+ */
+
+namespace App\Algorithmic\Sorts;
+
+
+class Sort
+{
+    private $data = [3,1,2,4,5,11,7,65,9];
+
+    public function bubble()
+    {
+        $cnt = count($this->data) - 1;
+        for($i = 0; $i <= $cnt; $i++) {
+            for($j = $i + 1; $j <= $cnt; $j++) {
+                if ($this->data[$i] > $this->data[$j]) {
+                    $tmp = $this->data[$i];
+                    $this->data[$i] = $this->data[$j];
+                    $this->data[$j] = $tmp;
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    public function select()
+    {
+        $cnt = count($this->data) - 1;
+        for($i = 0; $i <= $cnt; $i++) {
+            $p = $i;
+            for($j = $i+1; $j <= $cnt; $j++) {
+                if ($this->data[$p] > $this->data[$j]) {
+                    $p = $j;
+                }
+            }
+
+            if ($p != $i) {
+                $tmp = $this->data[$i];
+                $this->data[$i] = $this->data[$p];
+                $this->data[$p] = $tmp;
+            }
+        }
+
+        return $this;
+    }
+
+    public function insert()
+    {
+        $cnt = count($this->data);
+        for($i = 1; $i < $cnt; $i++) {
+            $val = $this->data[$i];
+            for($j = $i - 1; $j >= 0; $j--) {
+                if ($val < $this->data[$j]) {
+                    $this->data[$j+1] = $this->data[$j];
+                } else {
+                    break;
+                }
+            }
+
+            $this->data[$j + 1] = $val;
+        }
+
+        return $this;
+    }
+
+    public function binary($data)
+    {
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
+
+}
